e337bc6867ab24fc422203fe7ea4be49604616f7
Arthur-Gabrieel <gabrielarthur61584@gmail.com>
2024-11-28 07:57:22 -0300

    Add files via upload

---
+++ arthurgs/cod.js
@@ -0,0 +1,20 @@
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-images img');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100; // Muda a posição da imagem
+    document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
